a1200e20fdbbc30ac7c0c23e956f0765b047268b
MarioCaceres <mario.caceres@usach.cl>
2017-09-20 01:59:47 -0300

    Test SonarQube 1

--- src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
+++ src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
 
-//Test
+//Test1
 @Transactional
 public interface StudentController extends CrudRepository<Student, Long> {
 

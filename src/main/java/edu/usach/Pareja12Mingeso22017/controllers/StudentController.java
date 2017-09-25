4f324d6ec16a810b7a7bbf0c65b981e1a93e11ff
MarioCaceres <mario.caceres@usach.cl>
2017-09-25 01:14:14 -0300

    Prueba CI 6

--- src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
+++ src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
 
-//Test5
+//Test6
 @Transactional
 public interface StudentController extends CrudRepository<Student, Long> {
 

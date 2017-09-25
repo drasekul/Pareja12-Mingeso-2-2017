4bfb2c65f91fa0871faea78d598aaa0c5050394d
MarioCaceres <mario.caceres@usach.cl>
2017-09-25 04:07:52 -0300

    Test CI 13

--- src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
+++ src/main/java/edu/usach/Pareja12Mingeso22017/controllers/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
 
-//Test11
+//Test12
 @Transactional
 public interface StudentController extends CrudRepository<Student, Long> {
 
